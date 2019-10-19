fc43ac3ff2eaeced465b5ef1b46d87716aea1377
Arturo Fonseca <49870466+arturo32@users.noreply.github.com>
2019-10-18 21:54:22 -0300

    name change and another type

--- types.h
+++ types.h
@@ -1,10 +1,16 @@
-#ifndef TIPOS_H
-#define TIPOS_H
+#ifndef TYPES_H
+#define TYPES_H
 
 typedef struct{
     unsigned char R;
     unsigned char G;
     unsigned char B;
 }Pixel;
+
+typedef struct{
+	Pixel **image;
+	int x;
+	int y;
+}Image;
 
 #endif
