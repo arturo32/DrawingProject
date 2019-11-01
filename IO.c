91f41f38137eeee87a9d4e8a8514d2ff00891931
Arturo Fonseca <49870466+arturo32@users.noreply.github.com>
2019-10-31 23:16:22 -0300

    Organizando os comentários

--- IO.c
+++ IO.c
@@ -4,24 +4,25 @@
 #include <string.h>
 #include <stdio.h>
 
-#define MAGIC_NUMBER "P3" //PPM
+#define MAGIC_NUMBER "P3"
 #define MAX_VALUE 255
 
 
 Image newImage(FILE *fileTXT){
 	
-	int i, j, height, width;
-    Pixel pixel; //Pixel que será passado como parâmetro para a função clear
-    Image picture; //Imagem que será criada e manipulada
+	int i, height, width;
+    Image picture; 
 
     fscanf(fileTXT," %d %d\n",  &width, &height);
 
 	picture.height = height;
 	picture.width = width;
     
+    //Dynamicly allocating the matrix of Pixels
     picture.pixels = (Pixel**)(malloc(height * sizeof(Pixel*)));
     if(picture.pixels == NULL){
     	printf("Error in creating matrix\n");
+        //Stops the entire program
     	exit(EXIT_FAILURE);
     }
 
@@ -29,13 +30,12 @@ Image newImage(FILE *fileTXT){
     	picture.pixels[i] = (Pixel*)(malloc(width * sizeof(Pixel)));
     	if(picture.pixels[i] == NULL){
 			printf("Error in creating matrix\n");
-            //stops the entire program
+            //Stops the entire program
     		exit(EXIT_FAILURE);
     	}
     }
    
     return picture;
-
 }
 
 
@@ -43,29 +43,31 @@ void save(Image picture, FILE *fileTXT){
 
     FILE* ppmimg; 
     int i, j;
-    char imageName[10]; //Rever tamanho da variavel
+    char imageName[50];
     
-    fscanf(fileTXT, " %s\n", imageName); //Ler a função save e o nome da imagem ppm
+    //Reading the string that will be the name of the ppm file
+    fscanf(fileTXT, " %s\n", imageName); 
 
-    //Creating a file ready to be written with a name of "myimg.ppm"
+    //Creating the ppm file while it is opening for writing
     ppmimg = fopen(imageName, "w"); 
     if(ppmimg == NULL){
        printf("Error in creating ppm file.\n");
        exit(EXIT_FAILURE);
     }
   
-    // Writing Magic Number to the File 
+    //Writing Magic Number to the file 
     fprintf(ppmimg, "%s\n", MAGIC_NUMBER);  
   
-    // Writing the size of the image 
-    fprintf(ppmimg, "%d %d\n", picture.width, picture.height); //width and then height  
+    //Writing the size of the image 
+    fprintf(ppmimg, "%d %d\n", picture.width, picture.height); 
   
-    // Writing the maximum RGB value 
+    //Writing the maximum RGB value 
     fprintf(ppmimg, "%d\n", MAX_VALUE);
 
-    for (i = 0; i < picture.height; i++) { 
-        for (j = 0; j < picture.width; j++) {
-          // Writing the colour values in the 3D array to the file 
+    /*Writing the colour values in the matrix to the file while "freeing" the 
+    memory stored in the dynamic allocation*/
+    for (i = 0; i < picture.height; i++){ 
+        for (j = 0; j < picture.width; j++){
           fprintf(ppmimg, "%d %d %d ", picture.pixels[i][j].R, picture.pixels[i][j].G, picture.pixels[i][j].B); 
         } 
         free(picture.pixels[i]);
