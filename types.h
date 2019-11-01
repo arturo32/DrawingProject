7083ec692c40bce0aff79f2b7aad0bc57e139adf
Arturo Fonseca <49870466+arturo32@users.noreply.github.com>
2019-10-31 23:17:26 -0300

    Adicionando comentários

--- types.h
+++ types.h
@@ -1,12 +1,18 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+/*Type used to make a matrix of pixel that represents the image and
+to store the current color through out the entire code each value for
+a color is of the type unsigned char that have the same size of the
+maximum value for a color: 255*/
 typedef struct{
     unsigned char R;
     unsigned char G;
     unsigned char B;
 }Pixel;
 
+/*Type used to store a image formed by a matrix of Pixels and with
+height and width information*/
 typedef struct{
 	Pixel **pixels;
 	int height;
