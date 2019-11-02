7d1e72d022ddf583d547ea0660b586559212f8b0
Arturo Fonseca <49870466+arturo32@users.noreply.github.com>
2019-11-01 22:43:55 -0300

    Removendo fill de shapes

--- shapes.h
+++ shapes.h
@@ -9,7 +9,6 @@ Image line2(Image picture, Pixel* currentColor, int x0, int y0, int x1, int y1);
 Image line(Image picture, Pixel* currentColor, FILE* fileTXT);
 
 Image circle(Image picture, Pixel* currentColor, FILE* fileTXT);
-Image fill(Image picture, Pixel* currentColor, FILE* fileTXT);
 Image rect(Image picture, Pixel* currentColor, FILE* fileTXT);
 
 #endif
